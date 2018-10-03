3d4f89d93963bd0f56bff10f5d0f6dae4ba0031e
AngieSnyder <Angie.Snyder@protonmail.com>
2018-10-03 12:53:17 -0400

    intial commit

---
+++ src/PersonManagerApp.java
@@ -0,0 +1,21 @@
+
+public class PersonManagerApp {
+
+	public static void main(String[] args) {
+		
+		// Display Welcome Message		
+		System.out.println("Welcome to the Person Manager");
+		System.out.println();
+		
+		// Get user input
+		String chioce = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			String personType = Console.getString("Create customer or employee? (c/e): ","c","e");
+			System.out.println();
+						
+		}
+		
+
+	}
+
+}
